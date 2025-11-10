590627fa50b9f40f1c816f8d99d46897c6dcad24
KrashGL <joshwamah@gmail.com>
2025-11-10 19:32:05 +0000

    Added Input hookup to Windows

--- engine/src/core/application/application.c
+++ engine/src/core/application/application.c
@@ -21,6 +21,10 @@ typedef struct application_state {
 static b8 initialized = FALSE;
 static application_state app_state;
 
+// Event handlers
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+
 b8 application_create(game* game_inst) {
     if (initialized) {
         KERROR("application_create called more than once.");
@@ -40,6 +44,10 @@ b8 application_create(game* game_inst) {
         KERROR("Event system failed initialization. Application cannot continue.");
         return FALSE;
     }
+
+    event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
 
     if (!platform_startup(
             &app_state.platform,
@@ -96,10 +104,40 @@ b8 application_run() {
 
     app_state.is_running = FALSE;
 
+    // Shutdown the event system
+    event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
+
     event_shutdown();
     input_shutdown();
 
     platform_shutdown(&app_state.platform);
 
     return TRUE;
+}
+
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context) {
+    switch (code) {
+        case EVENT_CODE_APPLICATION_QUIT: {
+            KINFO("EVENT_CODE_APPLICATION_QUIT recieved, shutting down.\n");
+            app_state.is_running = FALSE;
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context) {
+    if (code == EVENT_CODE_KEY_PRESSED) {
+        // NOTE: Uncomment to show key pressed
+        // u16 key_code = context.data.u16[0];
+        // KDEBUG("'%c' key pressed in window.", key_code);
+    } else if (code == EVENT_CODE_KEY_RELEASED) {
+        // NOTE: Uncomment to show key released
+        // u16 key_code = context.data.u16[0];
+        // KDEBUG("'%c' key released in window.", key_code);
+    }
+    return FALSE;
 }
