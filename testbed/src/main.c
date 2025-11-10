e62dcc795c7d5b69c04d31e66fc017d2ff7a2675
KrashGL <joshwamah@gmail.com>
2025-11-10 18:39:31 +0000

    Added colorful Logging and Windows platform layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,13 +1,24 @@
 #include <core/logger/logger.h>
 
+// TODO: Remove Later
+#include <platform/platform.h>
+
 int main(void) {
-    // TODO: This is a test
+    // Test Logging
     KFATAL("A test message: %f", 3.14f);
     KERROR("A test message: %f", 3.14f);
     KWARN("A test message: %f", 3.14f);
     KINFO("A test message: %f", 3.14f);
     KDEBUG("A test message: %f", 3.14f);
     KTRACE("A test message: %f", 3.14f);
-    KASSERT(1 == 0);
+
+    platform_state state;
+    if(platform_startup(&state, "Krash Engine Testbed", 100, 100, 780, 480)) {
+        while(TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
+
     return 0;
 }
