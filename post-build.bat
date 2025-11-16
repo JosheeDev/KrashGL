cb16ce49fe2444b698349a0cbf4ed105263b0f09
KrashGL <joshwamah@gmail.com>
2025-11-16 09:08:00 +0000

    Added Resource/Loading system

--- post-build.bat
+++ post-build.bat
@@ -1,22 +1,13 @@
 @echo off
 
-REM Run from root directory!
-if not exist "%cd%\bin\assets\shaders\" mkdir "%cd%\bin\assets\shaders"
-
-if not exist "%cd%\bin\assets\shaders\material" mkdir "%cd%\bin\assets\shaders\material"
-
 echo "Compiling shaders..."
 
-echo "assets/shaders/material/Builtin.MaterialShader.vert.glsl -> bin/assets/shaders/material/Builtin.MaterialShader.vert.spv"
-%VULKAN_SDK%\bin\glslc.exe -fshader-stage=vert assets/shaders/material/Builtin.MaterialShader.vert.glsl -o bin/assets/shaders/material/Builtin.MaterialShader.vert.spv
+echo "assets/shaders/material/Builtin.MaterialShader.vert.glsl -> assets/shaders/material/Builtin.MaterialShader.vert.spv"
+%VULKAN_SDK%\bin\glslc.exe -fshader-stage=vert assets/shaders/material/Builtin.MaterialShader.vert.glsl -o assets/shaders/material/Builtin.MaterialShader.vert.spv
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
 
-echo "assets/shaders/material/Builtin.MaterialShader.frag.glsl -> bin/assets/shaders/material/Builtin.MaterialShader.frag.spv"
-%VULKAN_SDK%\bin\glslc.exe -fshader-stage=frag assets/shaders/material/Builtin.MaterialShader.frag.glsl -o bin/assets/shaders/material/Builtin.MaterialShader.frag.spv
+echo "assets/shaders/material/Builtin.MaterialShader.frag.glsl -> assets/shaders/material/Builtin.MaterialShader.frag.spv"
+%VULKAN_SDK%\bin\glslc.exe -fshader-stage=frag assets/shaders/material/Builtin.MaterialShader.frag.glsl -o assets/shaders/material/Builtin.MaterialShader.frag.spv
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
-
-echo "Copying assets..."
-echo xcopy "assets" "bin\assets" /h /i /c /k /e /r /y
-xcopy "assets" "bin\assets" /h /i /c /k /e /r /y
 
 echo "Done."
--- post-build.sh
+++ post-build.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-# Run from root directory!
-mkdir -p bin/assets
-mkdir -p bin/assets/shaders
-
-mkdir -p bin/assets/shaders/material
-
 echo "Compiling shaders..."
 
-echo "assets/shaders/material/Builtin.MaterialShader.vert.glsl -> bin/assets/shaders/material/Builtin.MaterialShader.vert.spv"
-$VULKAN_SDK/bin/glslc -fshader-stage=vert assets/shaders/material/Builtin.MaterialShader.vert.glsl -o bin/assets/shaders/material/Builtin.MaterialShader.vert.spv
+echo "assets/shaders/material/Builtin.MaterialShader.vert.glsl -> assets/shaders/material/Builtin.MaterialShader.vert.spv"
+$VULKAN_SDK/bin/glslc -fshader-stage=vert assets/shaders/material/Builtin.MaterialShader.vert.glsl -o assets/shaders/material/Builtin.MaterialShader.vert.spv
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]
 then
 echo "Error:"$ERRORLEVEL && exit
 fi
 
-echo "assets/shaders/material/Builtin.MaterialShader.frag.glsl -> bin/assets/shaders/material/Builtin.MaterialShader.frag.spv"
-$VULKAN_SDK/bin/glslc -fshader-stage=frag assets/shaders/material/Builtin.MaterialShader.frag.glsl -o bin/assets/shaders/material/Builtin.MaterialShader.frag.spv
+echo "assets/shaders/material/Builtin.MaterialShader.frag.glsl -> assets/shaders/material/Builtin.MaterialShader.frag.spv"
+$VULKAN_SDK/bin/glslc -fshader-stage=frag assets/shaders/material/Builtin.MaterialShader.frag.glsl -o assets/shaders/material/Builtin.MaterialShader.frag.spv
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]
 then
 echo "Error:"$ERRORLEVEL && exit
 fi
-
-echo "Copying assets..."
-echo cp -R "assets" "bin"
-cp -R "assets" "bin"
 
 echo "Done."
