1d190157e98aa6b140dbfb339c525308e5c71fec
KrashGL <joshwamah@gmail.com>
2025-11-18 16:40:27 +0000

    Added Shader system

--- post-build.bat
+++ post-build.bat
@@ -2,20 +2,20 @@
 
 echo "Compiling shaders..."
 
-echo "assets/shaders/material/Builtin.MaterialShader.vert.glsl -> assets/shaders/material/Builtin.MaterialShader.vert.spv"
-%VULKAN_SDK%\bin\glslc.exe -fshader-stage=vert assets/shaders/material/Builtin.MaterialShader.vert.glsl -o assets/shaders/material/Builtin.MaterialShader.vert.spv
+echo "assets/shaders/Builtin.MaterialShader.vert.glsl -> assets/shaders/Builtin.MaterialShader.vert.spv"
+%VULKAN_SDK%\bin\glslc.exe -fshader-stage=vert assets/shaders/Builtin.MaterialShader.vert.glsl -o assets/shaders/Builtin.MaterialShader.vert.spv
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
 
-echo "assets/shaders/material/Builtin.MaterialShader.frag.glsl -> assets/shaders/material/Builtin.MaterialShader.frag.spv"
-%VULKAN_SDK%\bin\glslc.exe -fshader-stage=frag assets/shaders/material/Builtin.MaterialShader.frag.glsl -o assets/shaders/material/Builtin.MaterialShader.frag.spv
+echo "assets/shaders/Builtin.MaterialShader.frag.glsl -> assets/shaders/Builtin.MaterialShader.frag.spv"
+%VULKAN_SDK%\bin\glslc.exe -fshader-stage=frag assets/shaders/Builtin.MaterialShader.frag.glsl -o assets/shaders/Builtin.MaterialShader.frag.spv
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
 
-echo "assets/shaders/ui/Builtin.UIShader.vert.glsl -> assets/shaders/ui/Builtin.UIShader.vert.spv"
-%VULKAN_SDK%\bin\glslc.exe -fshader-stage=vert assets/shaders/ui/Builtin.UIShader.vert.glsl -o assets/shaders/ui/Builtin.UIShader.vert.spv
+echo "assets/shaders/Builtin.UIShader.vert.glsl -> assets/shaders/Builtin.UIShader.vert.spv"
+%VULKAN_SDK%\bin\glslc.exe -fshader-stage=vert assets/shaders/Builtin.UIShader.vert.glsl -o assets/shaders/Builtin.UIShader.vert.spv
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
 
-echo "assets/shaders/ui/Builtin.UIShader.frag.glsl -> assets/shaders/ui/Builtin.UIShader.frag.spv"
-%VULKAN_SDK%\bin\glslc.exe -fshader-stage=frag assets/shaders/ui/Builtin.UIShader.frag.glsl -o assets/shaders/ui/Builtin.UIShader.frag.spv
+echo "assets/shaders/Builtin.UIShader.frag.glsl -> assets/shaders/Builtin.UIShader.frag.spv"
+%VULKAN_SDK%\bin\glslc.exe -fshader-stage=frag assets/shaders/Builtin.UIShader.frag.glsl -o assets/shaders/Builtin.UIShader.frag.spv
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
 
 echo "Done."
