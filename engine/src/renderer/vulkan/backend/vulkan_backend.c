a5ddfd6df63bfe84efdfb7a112710f644e401854
KrashGL <joshwamah@gmail.com>
2025-11-22 12:35:41 +0000

    Added job system

--- engine/src/renderer/vulkan/backend/vulkan_backend.c
+++ engine/src/renderer/vulkan/backend/vulkan_backend.c
@@ -169,6 +169,9 @@ b8 vulkan_renderer_backend_initialize(renderer_backend* backend, const renderer_
     VK_CHECK(vkCreateInstance(&create_info, context.allocator, &context.instance));
     KINFO("Vulkan Instance created.");
 
+    // TODO: implement multi-threading.
+    context.multithreading_enabled = false;
+
     // Debugger
 #if defined(_DEBUG)
     KDEBUG("Creating Vulkan debugger...");
@@ -2037,9 +2040,15 @@ texture* vulkan_renderer_window_attachment_get(u8 index) {
 
     return context.swapchain.render_textures[index];
 }
+
 texture* vulkan_renderer_depth_attachment_get() {
     return context.swapchain.depth_texture;
 }
+
 u8 vulkan_renderer_window_attachment_index_get() {
     return (u8)context.image_index;
+}
+
+b8 vulkan_renderer_is_multithreaded() {
+    return context.multithreading_enabled;
 }
--- engine/src/resources/loaders/mesh/mesh_loader.c
+++ engine/src/resources/loaders/mesh/mesh_loader.c
@@ -291,6 +291,7 @@ b8 import_obj_file(file_handle* obj_file, const char* out_ksm_filename, geometry
     char material_file_name[512] = "";
 
     char name[512];
+    kzero_memory(name, sizeof(char) * 512);
     u8 current_mat_name_count = 0;
     char material_names[32][64];
 
@@ -502,6 +503,7 @@ b8 import_obj_file(file_handle* obj_file, const char* out_ksm_filename, geometry
     if (string_length(material_file_name) > 0) {
         // Load up the material file
         char full_mtl_path[512];
+        kzero_memory(full_mtl_path, sizeof(char) * 512);
         string_directory_from_path(full_mtl_path, out_ksm_filename);
         string_append_string(full_mtl_path, full_mtl_path, material_file_name);
 
